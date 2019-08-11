ac80d7cbc349d16fbb2979c30a42a4f6978dd0fe
michaelstresing <mstresing@gmail.com>
2019-08-11 15:09:45 +0100

    Starting work on pdfreader

--- pdfreader/pdfreader.py
+++ pdfreader/pdfreader.py
@@ -1,3 +1,129 @@
+from tika import parser
+
+
+def get_words(document):
+    """
+    Gets a list of words within the document, then sets them all to lowercase
+    :param document:
+    :return: a list of words in the PDF as all lowercase
+    """
+
+    raw = parser.from_file(document)
+    docwords = raw['content'].split()
+    print(docwords)
+
+    lowercasewords = []
+
+    for word in docwords:
+        lowercasewords.append(word.lower())
+
+    print(lowercasewords)
+    return lowercasewords
+
+def Check_for_financial_activity(list):
+    """
+    Checks the word list from get_words for potential issue words relating to fintech companies.
+    :param list:
+    :return: Flag message for fintech
+    """
+
+    flagwords = ['fca',
+                 'exchange',
+                 'insurance',
+                 'freetrade',
+                 'robinhood',
+                 'securities',
+                 'algorithmic trading',
+                 'insure', 'financial',
+                 'professional services',
+                 'roboadvisor',
+                 'fintech']
+
+    for word in list:
+        if word in flagwords:
+            print('There maybe financial activity present.')
+        else:
+            print('.')
+
+
+def Check_for_sufficient_risks(list):
+    """
+    Checks the word list from get_words for if any words relate to R2C condition.
+    :param list:
+    :return: Flag message for risks
+    """
+
+    flagwords = ['risks',
+                 'weaknesses',
+                 'threats',
+                 'swot',
+                 ]
+
+    for word in list:
+        if word not in flagwords:
+            print('There is unlikely any presentation of risks.')
+        else:
+            print('.')
+
+
+def Check_for_blockchain(list):
+    """
+    Checks the word list from get_words for potential issue words relating to blockchain companies.
+    :param list:
+    :return: Flag message for blockchain
+    """
+
+    flagwords = ['dlt',
+                 'etherium',
+                 'blockchain',
+                 'ledger',
+                 'tokenization',
+                 'tokens',
+                 ]
+
+    for word in list:
+        if word not in flagwords:
+            print('There may be blockchain related issues.')
+        else:
+            print('.')
+
+def Check_for_groups(list):
+    """
+    Checks the word list from get_words for potential issue words relating to group structures.
+    :param list:
+    :return: Flag message for groups
+    """
+
+    flagwords = ['group',
+                 'subsidiary',
+                 'subsidiaries',
+                 'topco',
+                 ]
+
+    for word in list:
+        if word not in flagwords:
+            print('There may be group structure related issues.')
+        else:
+            print('.')
+
+
+'''current issues + features: 
+
+    1. structuring the program better, so that it's OOP? (A function to check, with decotrators or sub-functions which 
+    modify the text dependant on the trigger words.
+    
+    2. Setting up the input so that it can be run on any pdf file. 
+    
+    3. Setting up the output so that it flags where the word was, for example as an MVP, it could output where the
+    flagged word was in the list, as a percentage of the way through the document?. 
+    
+    4. Writing tests. 
+'''
+
+# ==================
+
+''' Initial work was using PyPDF2 - however, accuracy and utility appears worse than tika
+
 import PyPDF2
 
 
@@ -35,3 +161,4 @@ def get_text():
 
 
 print(get_text())
+'''
